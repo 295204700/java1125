083be16ede4ea31a7a3b17d1f3635f5b4d445237
lpy <295204700@qq.com>
2021-01-23 14:58:22 +0800

    GIT第一次提交

---
+++ src/com/sy/MyTest.java
@@ -0,0 +1,15 @@
+package com.sy;
+
+/**
+ * @Title: MyTest
+ * @Description:
+ * @Auther:
+ * @Version: 1.0
+ * @create 2021/1/23 0023 14:24
+ */
+public class MyTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
