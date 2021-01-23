8c2e05a1f376fa527ecb9a4a632f21d09cc6cdb2
lpy <295204700@qq.com>
2021-01-23 14:59:52 +0800

    GIT第三次提交

--- src/com/sy/MyTest.java
+++ src/com/sy/MyTest.java
@@ -12,5 +12,6 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("第二次提交");
+        System.out.println("第三次提交");
     }
 }
